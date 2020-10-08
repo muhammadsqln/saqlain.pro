931f812edb06f1e9303ef17b687639015ab1eec0
muhammadsqln <44453366+muhammadsqln@users.noreply.github.com>
2020-10-08 00:22:29 -0400

    initial template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function Cover() {
+    return <div id="cover">
+
+        <h1 class="cover-text">Muhammad Saqlain</h1>
+        <h2 class="cover-text">Toronto, Canada</h2>
+        <h2 class="cover-text">(647) 786-6803</h2>
     </div>
-  );
+
+}
+
+
+
+function Navigator(count) {
+    return <div id="navigator">
+        <Dot selected={false} />
+        <Dot selected={true} />
+        <Dot selected={false} />
+        <Dot selected={false} />
+        <Dot selected={false} />
+    </div>
+}
+
+function Dot(props) {
+
+    const className = props.selected === true ? "dot selected" : "dot";
+
+    return <div className={className}>
+
+    </div>
+
+}
+
+
+function DetailPane() {
+    return <div id="detail-pane">
+
+<DetailTitle title="Experience" />
+
+
+
+    </div>
+}
+
+function DetailTitle(props) {
+
+    return <div id="title-header">
+        <div id="detail-title">
+            {props.title}
+        </div>
+    </div>
+
+}
+
+
+
+
+function App() {
+    return <div id="home">
+        <Cover />
+        <Navigator />
+        <DetailPane />
+
+
+
+
+
+
+    </div>;
 }
 
 export default App;
